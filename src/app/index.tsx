d2a6daf6128375e80c4e9cacb43bbad45f94258e
Svetlana Trukhina <sweta4ca@mail.ru>
2023-10-27 21:15:27 +0700

    feat: add base structure, normolize style

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import '../shared/styles/global.css';
+
+import { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return <div>App component</div>;
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const domNode = document.getElementById('root')!;
+const root = createRoot(domNode);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
