8061483d8ee300234a66a37ee836650bb2d3f781
Svetlana Trukhina <sweta4ca@mail.ru>
2023-11-01 21:50:03 +0700

    refactor: add to allow console error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -39,7 +39,7 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
-    'no-console': 'error',
+    'no-console': ['error', { allow: ['error'] }],
   },
   settings: {
     react: {
