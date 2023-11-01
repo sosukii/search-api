c85c0cbc27e34b4b776f0a6bbdbe44d8f58a1218
Svetlana Trukhina <sweta4ca@mail.ru>
2023-11-02 06:31:51 +0700

    feat: rewrite SearchBar from class to function

--- src/features/search-bar/ui/SearchBar.tsx
+++ src/features/search-bar/ui/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { InputSearch } from '@shared/ui/InputSearch/InputSearch';
 import { Button } from '@shared/ui/Button/Button';
 import css from './style.module.css';
@@ -8,14 +8,12 @@ interface Props {
   onClickNewName?: () => void;
   inputValue: string;
 }
-export class SearchBar extends Component<Props> {
-  render() {
-    const { onInputNewName, onClickNewName, inputValue } = this.props;
-    return (
-      <div className={css.bar}>
-        <InputSearch onChange={onInputNewName} inputValue={inputValue} />
-        <Button onClick={onClickNewName}>Go search!</Button>
-      </div>
-    );
-  }
-}
+
+export const SearchBar: FC<Props> = ({ onInputNewName, onClickNewName, inputValue }) => {
+  return (
+    <div className={css.bar}>
+      <InputSearch onChange={onInputNewName} inputValue={inputValue} />
+      <Button onClick={onClickNewName}>Go search!</Button>
+    </div>
+  );
+};
