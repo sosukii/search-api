0a8ecc99301d7b459b17ed4170f0ddb583f28e0e
Svetlana Trukhina <sweta4ca@mail.ru>
2023-11-02 02:00:42 +0700

    feat: rewrite Loader from class to function

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -1,18 +1,14 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import css from './style.module.css';
 
-export class Loader extends Component {
-  state = {};
-
-  render() {
-    return (
-      <div className={css.loader}>
-        <div className={css.dot}></div>
-        <div className={css.dot}></div>
-        <div className={css.dot}></div>
-        <div className={css.dot}></div>
-        <div className={css.dot}></div>
-      </div>
-    );
-  }
-}
+export const Loader: FC = () => {
+  return (
+    <div className={css.loader}>
+      <div className={css.dot}></div>
+      <div className={css.dot}></div>
+      <div className={css.dot}></div>
+      <div className={css.dot}></div>
+      <div className={css.dot}></div>
+    </div>
+  );
+};
